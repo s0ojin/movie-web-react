697c44b99ba05c6bf92606afe41fc1893b6f092c
s0ojin <onepst@hanyang.ac.kr>
2022-04-07 22:44:25 +0900

    add todo

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import {useState} from "react";
+
+function App() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value)
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if(toDo === "") {
+      return;
+    }
+    setToDo("");
+    setToDos(currentArray => [toDo, ...currentArray]);
+  }
+    return (
+      <div>
+        <form onSubmit={onSubmit}>
+          <h1>My To Dos({toDos.length})</h1>
+          <input onChange={onChange} value={toDo} type="text" placeholder="Write your to do..."/>
+          <button>Add To Do</button>
+        </form>
+        <hr />
+        <ul>
+          {toDos.map((item, index)=><li key={index}>{item}</li>)}
+        </ul>
+      </div>
+      
+  )
+}
+export default App;
