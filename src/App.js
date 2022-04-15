7ce0bb2a78237fd1c29583aa5a82828fa0c3da4a
s0ojin <onepst@hanyang.ac.kr>
2022-04-15 23:19:29 +0900

    add movie

--- src/App.js
+++ src/App.js
@@ -1,35 +1,23 @@
-import {useState, useEffect} from "react";
+import Home from "./routes/Home";
+import Detail from "./components/Detail";
+
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
+
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [top, setTop] = useState([]);
-  useEffect(()=>{
-    fetch("https://api.coinpaprika.com/v1/tickers?quotes=KRW")
-    .then(response => response.json())
-    .then(json=>{
-      setCoins(json);
-      setTop(json.slice(0,100));
-      setLoading(false);
-    });
-  },[])
   return (
-      <div>
-        <h1>The coins! ({coins.length})</h1>
-        {loading ? <strong>Loading...</strong> :
-          <select>
-            {coins.map(coin=>
-            <option key={coin.id}>{coin.name} ({coin.symbol}) {(Math.round(coin.quotes.KRW.price)).toLocaleString()} KRW</option>
-            )}
-          </select>
-        }
-        <h2>실시간 TOP100</h2>
-        <ol>
-        {top.map(coin=>
-            <li key={coin.id}>{coin.name} ({coin.symbol}) {(Math.round(coin.quotes.KRW.price)).toLocaleString()} KRW</li>
-            )}
-        </ol>
-      </div>
-  )
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/movie" element={<Detail />} />
+    </Routes>
+  </BrowserRouter>
+  );
 }
+
 export default App;
